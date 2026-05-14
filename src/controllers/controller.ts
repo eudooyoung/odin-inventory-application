ddf6c51e0dd3f93768c29da8f316904dd5128433
Kim Doo Young <177651948+eudooyoung@users.noreply.github.com>
2026-05-14 17:31:24 +0900

    feat: wip style refactor

--- src/controllers/controller.ts
+++ src/controllers/controller.ts
@@ -1,11 +1,6 @@
 import type types = require("../utils/types");
 import db = require("../db/queries");
-import validateRule = require("../utils/validator");
-import validator = require("express-validator");
-import jsConvert = require("js-convert-case");
 import links = require("../utils/links");
-const { validateCategory, validateProduct } = validateRule;
-const { validationResult, matchedData } = validator;
 
 const homeGet: types.ControllerMiddleware = async (req, res) => {
   const categories = await db.getAllCategories();
@@ -19,39 +14,4 @@ const homeGet: types.ControllerMiddleware = async (req, res) => {
   });
 };
 
-const newGet: types.ControllerMiddleware = async (req, res) => {
-  const categories = await db.getAllCategories();
-  const products = await db.getAllProducts();
-  res.render("new", {
-    title: "New inventory",
-    links: links,
-    categories: categories,
-  });
-};
-
-const newProductPostMiddleware: types.ControllerMiddleware = async (
-  req,
-  res,
-) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    const categories = await db.getAllCategories();
-    const products = await db.getAllProducts();
-    return res.status(400).render("new", {
-      title: "New inventory",
-      links: links,
-      categories: categories,
-      products: products,
-      productErrors: errors.array(),
-      prev: req.body,
-    });
-  }
-  const data = matchedData(req);
-  const coverted = jsConvert.camelKeys(data) as types.ProductInput;
-  await db.insertProduct(coverted);
-  res.redirect("/");
-};
-
-const newProductPost = [...validateProduct, newProductPostMiddleware];
-
-export = { homeGet, newGet,  newProductPost };
+export = { homeGet };
