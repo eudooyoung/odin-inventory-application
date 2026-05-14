7dfc5bc0af9fabf150498823000e2c8f162c2ab9
Kim Doo Young <177651948+eudooyoung@users.noreply.github.com>
2026-05-14 17:53:34 +0900

    refactor: add styles and refactore views structure

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,7 @@ const PORT = process.env.PORT;
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "/public")));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", router);
 app.use("/category", categoryRouter);
