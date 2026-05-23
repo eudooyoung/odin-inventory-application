7cb90fa2e22b9fb81b17ce201e0e1ed7602dddc4
Kim Doo Young <177651948+eudooyoung@users.noreply.github.com>
2026-05-23 21:49:23 +0900

    feat: log time spend during queirs on page home

--- src/controllers/controller.ts
+++ src/controllers/controller.ts
@@ -2,9 +2,11 @@ import type types = require("../utils/types");
 import db = require("../db/queries");
 
 const homeGet: types.Middleware = async (req, res) => {
+  console.time("db all");
   const categories = await db.getAllCategories();
   const products = await db.getAllProducts();
   const options = await db.getAllOptions();
+  console.timeEnd("db all");
   res.render("index", {
     route: { page: "home" },
     categories: categories,
